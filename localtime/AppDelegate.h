3b2f840a6f022f7fd4cfd5bffcc21218820de8e9
Jeff Grimes <jeffgrimes9@gmail.com>
2012-09-29 00:43:42 -0400

    Initial Commit

---
+++ localtime/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  localtime
+//
+//  Created by Jeff Grimes on 9/29/12.
+//  Copyright (c) 2012 Jeff Grimes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ localtime/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  localtime
+//
+//  Created by Jeff Grimes on 9/29/12.
+//  Copyright (c) 2012 Jeff Grimes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
